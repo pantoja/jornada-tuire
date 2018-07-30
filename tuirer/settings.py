b1be6e8749bcea73108062e9399bf1f9d97e426b
Victoria Pantoja do Amaral <vpa@ug4c15.windows.cin.ufpe.br>
2018-07-30 17:24:18 -0300

    dia 6

--- tuirer/settings.py
+++ tuirer/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'core',
     'tuites',
     'users',
+
+    #3rd party
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -85,7 +88,7 @@ DATABASES = {
 
 # Users
 AUTH_USER_MODEL = 'users.User'
-
+LOGIN_URL = 'index'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
--- tuites/views.py
+++ tuites/views.py
@@ -1,24 +1,50 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from tuites.forms import PostTuiteForm
 from tuites.models import Tuite
 
-def post_tuite(request):
-    context = {}
+class PostTuiteView(LoginRequiredMixin, CreateView):
+    model = Tuite
+    template_name = 'post_tuite.html'
+    form_class = PostTuiteForm
+    success_url = reverse_lazy('post_tuite')
+    
+    def get_initial(self):
+        return{
+            'user': self.request.user
+        }
+    
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            'Você postou um tuire'
+        )
+        return super().form_valid(form)
 
-    if request.method == 'POST':
-        print('Enviando')
-        # print(request.POST) retorna um dicionário
-        content = request.POST.get('content', None)
-        
-        if content == '' or content == ' ':
-            context['error'] = 'Tuite não pode estar vazio'
-        
-        else:
-            tuite =  Tuite.objects.create(
-                content = content,
-                author = request.user,
-            )
-            context['success_message'] = f'O tweet "{tuite.content}" foi enviado'
-        
-        print(content)
+# Não está sendo utilizado
 
-    return render(request, 'post_tuite.html', context)
+# def post_tuite(request):
+#     context = {}
+
+#     if request.method == 'POST':
+#         print('Enviando')
+#         # print(request.POST) retorna um dicionário
+#         content = request.POST.get('content', None)
+        
+#         if content == '' or content == ' ':
+#             context['error'] = 'Tuite não pode estar vazio'
+        
+#         else:
+#             tuite =  Tuite.objects.create(
+#                 content = content,
+#                 author = request.user,
+#             )
+#             context['success_message'] = f'O tweet "{tuite.content}" foi enviado'
+        
+#         print(content)
+
+#     return render(request, 'post_tuite.html', context)
